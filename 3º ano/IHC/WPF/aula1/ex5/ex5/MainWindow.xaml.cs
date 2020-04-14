8023afc25c629642e4e3f59052aa6b42e3e3a143
pedralmeida22 <pedro22@ua.pt>
2020-04-14 15:18:18 -0700

    after pascoa

---
+++ 3º ano/IHC/WPF/aula1/ex5/ex5/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ex5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.RightButton == MouseButtonState.Pressed)
+            {
+                Console.WriteLine("Botão direito");
+            }
+        }
+
+        private void Window_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Point p = e.GetPosition(this);
+            double x = p.X;
+            double y = p.Y;
+            Console.WriteLine("({0}, {1})", x, y);
+        }
+    }
+}
